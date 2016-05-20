34947e2bbc35699f3f0b2fc89bf6c1037f56d1c6
David Brambila Ventura <davidv@softclouds.com>
2016-05-19 17:01:26 -0700

    Deleted Utility Class

--- GCMApp/app/src/main/java/com/softclouds/gcmapp/MainActivity.java
+++ GCMApp/app/src/main/java/com/softclouds/gcmapp/MainActivity.java
@@ -9,12 +9,9 @@ import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
-import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -24,9 +21,6 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.softclouds.gcmapp.utility.Mail;
-import com.softclouds.gcmapp.utility.Utility;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -98,7 +92,7 @@ public class MainActivity extends Activity {
             }
         };
 
-       // Utility.postMethod("davidv@softclouds.com", "sending token here", this);
+
     }
 
     private boolean checkPlayServices() {
